28e0186e3fd1615239954929f28787037cc07a3e
Bobby Williams <bobbylw@amazon.com>
2024-04-09 22:09:05 -0400

    adding script to deploy help charts

--- charts-deploy.sh
+++ charts-deploy.sh
@@ -6,19 +6,18 @@
 Help()
 {
    # Display Help
-   echo "Uploads charts from the charts folder to gitlab."
+   echo "Uploads helm chart contained in the list of directories to a registry"
    echo
-   echo "Syntax: scriptTemplate [-r|t|p|u]"
+   echo "Syntax: scriptTemplate [-r|-p|-u]"
    echo "options:"
-   echo "r     The git helm package registry location that contains the helm charts for the project"
-   echo "t     The the personal access token or the deploy token which allows registration of the helm charts"
-   echo "p     The project id for the helm repository"
-   echo "u     The username for the token"
+   echo "r     The helm registry where the charts will be uploaded"
+   echo "p     The password to log into the helm registry"
+   echo "u     The username to log into the helm registry"
    echo
 }
 
 # Get the options
-while getopts ":h:r:t:p:u:" option; do
+while getopts ":h:r:p:u:" option; do
    case $option in
       h) # display Help
          Help
@@ -28,15 +27,17 @@ while getopts ":h:r:t:p:u:" option; do
 	 Help
          exit;;
       r) # The git helm package registry location that contains the helm charts for the projects
-	 REPO=${OPTARG};;
-      t) # The the personal access token or the deploy token which allows registration of the helm charts
-	 TOKEN=${OPTARG};;
-      p) # The project id for the helm repository
-	 PROJECT_ID="${OPTARG}";;
-      u) # The username for the token
+	 REGISTRY=${OPTARG};;
+      p) # The password for the registry
+	 PASSWORD=${OPTARG};;
+      u) # The username for the registry
     USERNAME="${OPTARG}";;
    esac
 done
+
+base_dir=$(pwd)
+
+helm registry login ${REGISTRY} --username ${USERNAME} --password ${PASSWORD}
 
 CHARTS_PATH="./charts"
 cd ${CHARTS_PATH}
@@ -50,17 +51,33 @@ done
 echo "There are ${#dirs[@]} charts in the current path"
 for((i=1;i<=${#dirs[@]};i++))
 do
-  directory="${dirs[i]}"
+   directory="${dirs[i]}"
 
-  chart_name="${directory}"
+   cd ${base_dir}/${CHARTS_PATH}/${directory}
 
-  echo ${REPO}
-  echo ${chart_name}
-  echo ${USERNAME}
-  echo ${TOKEN}
+   # get the directory base name
+   directory_name=$(basename "${directory}")
 
-  helm repo add ${chart_name} --username ${USERNAME} --password ${TOKEN} ${REPO}
+   # read the name attribute from the chart.yaml file
+   chart_name=$(yq '.name' Chart.yaml)
 
-  helm package ${chart_name}
-  helm cm-push ${chart_name}*.tgz ${chart_name}
+   # read the version attribute from the chart.yaml file
+   chart_version=$(yq '.version' Chart.yaml)
+
+   cd ..
+
+   REPO_EXISTS=$(aws ecr describe-repositories | jq -r --arg repoName "${chart_name}" '.repositories[] | select( .repositoryName == $repoName )' | jq length)
+
+   if [ -z "${REPO_EXISTS}" ]
+   then
+      aws ecr create-repository --repository-name "${chart_name}" --region "us-east-1" 1> /dev/null    
+   fi
+
+   echo "Creating helm chart: ${chart_name} with version: ${chart_version} and pushing to registry: ${REGISTRY}"
+
+   helm package ${directory_name}
+   helm push ${chart_name}-${chart_version}.tgz "oci://${REGISTRY}/"
+
 done
+
+cd ${base_dir}
